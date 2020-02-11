48400dbd38eedcbecda388750bfcafd061975a71
xzero <xzero@debian.X>
2020-02-11 07:09:00 -0600

    cambios al modulo avances en las applicaciones y vistas, ademas de estructura

--- xtest_module/models/__init__.py
+++ xtest_module/models/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import models, fields, api
+
+class Course(models.Model):
+    _name = 'xtest.course'
+    _description = "xtest module creation for Odoo development"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
